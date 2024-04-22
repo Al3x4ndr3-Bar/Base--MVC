6b7b44c76708b6d224c28640c29a5c81406511f1
BARBIER Alexandre <146002173+Al3x4ndr3-Bar@users.noreply.github.com>
2024-04-22 19:52:03 +0200

    Modification faite par GPT-4 pour conprendre les controller, Repository, affichage (méme si c'est super claire pour linstant)...

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository; // Import ajouté ici
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/users', name: 'user_list')]
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $users,
         ]);
     }
 }
